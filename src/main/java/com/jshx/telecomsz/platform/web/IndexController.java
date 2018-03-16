512d2a2bcc87399634733453e5fb97bc12b83404
kdsF <1542862571@qq.com>
2018-03-16 17:27:44 +0800

    1 2

--- src/main/java/com/jshx/telecomsz/platform/web/IndexController.java
+++ src/main/java/com/jshx/telecomsz/platform/web/IndexController.java
@@ -1,10 +1,17 @@
 package com.jshx.telecomsz.platform.web;
 
 import com.jshx.telecomsz.platform.inerface.DataInterface;
+import com.jshx.telecomsz.platform.model.Building;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+import static org.springframework.util.StringUtils.isEmpty;
 
 /**
  * Created with Intellij IDEA by 王金 on 2018/3/9 10:51.
@@ -20,11 +27,20 @@ public class IndexController {
 
 
     @GetMapping(value = "/")
-    public ModelAndView index() {
+    public ModelAndView index(@RequestParam(value = "buildingCode",required = false) Integer buildingCode) {
 
+        if (buildingCode == null || buildingCode == 0 || buildingCode.equals(0)) {
+            buildingCode = 1;
+        }
 
         ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("data", dataInterface.getBuildingData());
+        List<Building> buildingData = dataInterface.getBuildingData();
+        for (Building building : buildingData) {
+            if (building.getBuildingCode().equals(buildingCode)) {
+                modelAndView.addObject("building", building);
+            }
+        }
+
         return modelAndView;
     }
 
