452763b36a77deee65bf7437b692378df24f1636
kdsF <1542862571@qq.com>
2018-03-28 17:35:28 +0800

    桌面通知点击事件回调

--- src/main/resources/static/js/socketApp.js
+++ src/main/resources/static/js/socketApp.js
@@ -7,7 +7,14 @@ stompClient.connect({}, function (frame) { //连接
     stompClient.subscribe('/device/test', function (greeting) { //订阅，监听收到消息
 
         console.log('收到消息', greeting.body);
-        showNotification('苏州电信智慧物联网', greeting.body); //桌面弹窗提醒 依赖notification.js
+
+        //桌面通知 依赖notification.js @param 标题，消息内容，点击事件的回调发法
+        showNotification('苏州电信智慧物联网', greeting.body, function (notify) {
+            window.focus(); //激活所在窗口
+            setTimeout(function () { //延时主动关闭通知消息
+                notify.close();
+            }, 2000);
+        });
 
     });
 });
